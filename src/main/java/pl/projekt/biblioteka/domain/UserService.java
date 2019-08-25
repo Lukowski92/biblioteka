469e785648712be4b03207fc4472b9ad321891da
Radek Mazurek <radekmazurek@gmail.com>
2019-08-25 09:40:09 +0200

    User do dokończenia

--- src/main/java/pl/projekt/biblioteka/domain/UserService.java
+++ src/main/java/pl/projekt/biblioteka/domain/UserService.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Service;
 import pl.projekt.biblioteka.infrastructure.dto.UserDto;
 import pl.projekt.biblioteka.infrastructure.entity.User;
 import pl.projekt.biblioteka.infrastructure.repositories.UserRepository;
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,29 +13,23 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    @Autowired
-    private final UserRepository userRepository;
+  @Autowired private final UserRepository userRepository;
 
+  public void createOrUpdate(UserDto dto) {
+    User user =
+        User.builder()
+            .id(dto.getId())
+            .firstName(dto.getFirstName())
+            .lastName(dto.getLastName())
+            .build();
+    userRepository.save(user);
+  }
 
-    public void createOrUpdate(UserDto dto) {
-        User user = User.builder()
-                .id(dto.getId())
-                .firstName(dto.getFirstName())
-                .cathegory(dto.getLastName())
-                .build();
-        userRepository.save(user);
-    }
+  public void delete(Long userId) {
+    userId.deleteById(userId);
+  }
 
-    public void delete(Long userId) {
-        userId.deleteById(userId);
-    }
-
-    public List<UserDto> findAllUsers() {
-        return UserDto.findAll()
-                .stream()
-                .map(User::toDto)
-                .collect(Collectors.toList());
-    }
-
-
+  public List<UserDto> findAllUsers() {
+    return UserDto.findAll().stream().map(User::toDto).collect(Collectors.toList());
+  }
 }
